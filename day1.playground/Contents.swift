8eb35ebc5b773f6771e30975e3574846391bfb21
C36-HussainHaidarAlmousawi <70634196+C36-HussainHaidarAlmousawi@users.noreply.github.com>
2020-09-09 20:14:39 +0300

    done

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,21 @@
+
+let name = "hussain almousawi"
+var hobby2 = "basketball"
+var hobby1 = "coding"
+var age = 14
+var height = 180
+
+print("my name is \(name) and my age is: \(age) and i love \(hobby2 + "," + hobby1) and im \(height)cm")
+
+let friend_name = "ali"
+var friend_hobby2 = "ribix cube"
+var friend_hobby1 = "basketball"
+var friend_age = 15
+var friend_height = 165
+
+var age_diffrence = age - friend_age
+age > friend_age
+name == friend_name
+
+var mutrall_hobby = (hobby1 == friend_hobby1) || (hobby1 == friend_hobby2) || (hobby2 == friend_hobby1) || (hobby2 == friend_hobby2)
+print(mutrall_hobby)
